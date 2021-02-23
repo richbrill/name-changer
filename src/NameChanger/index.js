16b463f9da16dddfb9c128d65b05848934651aa9
richbrill <3015680+richbrill@users.noreply.github.com>
2021-02-23 21:47:55 +0000

    Added useEffect to call /GET endpoint, tweaked formatting

--- src/NameChanger/index.js
+++ src/NameChanger/index.js
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { TextInput } from '@instructure/ui-text-input';
 import { Button } from '@instructure/ui-buttons';
+import { View } from '@instructure/ui-view';
+import styles from './styles.css';
+import { API } from '../constants';
 
 const NameChanger = () => {
   const [name, setName] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(`${API}/get`);
+        if (res.status === 200) {
+          const json = await res.json();
+          setName(json.name);
+        }
+      } catch (err) {
+        console.warn('err', err);
+      }
+    })();
+  }, []);
   
   return (
-    <>
-      <TextInput renderLabel="Name" id="name" type="text" value={name} onChange={e => setName(e.target.value)}/>
+    <View margin="small" display="inline-block">
+      <TextInput
+        renderLabel="Name"
+        id="name"
+        type="text"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
       <Button color="primary" margin="small">Save</Button>
-    </>
+    </View>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const API = 'http://simple-endpoint-dev.eu-west-1.elasticbeanstalk.com';
